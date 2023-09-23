acfabcf32598ae79a75de6e272789d39c9f53448
Jesse Ajioh <ajiohjesse@gmail.com>
2023-09-23 02:53:59 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,173 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+} from '@/components/ui/card';
+import {
+  DialogHeader,
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="w-full bg-background">
+      <section className="container py-16 space-y-8 max-w-[400px] sm:max-w-[600px] md:space-y-12">
+        <div className="space-y-4 text-center md:space-y-8">
+          <h1 className="text-3xl font-semibold sm:text-4xl md:text-5xl leading-normal sm:leading-relaxed md:leading-[4rem]">
+            Send Messages Anonymously
+            <span className="text-primary">.</span>
+          </h1>
+          <p className="text-muted text-sm md:text-base">
+            Have you ever wanted to speak your mind without
+            fear of judgment or repercussions? With
+            Incognito, you can finally express yourself
+            freely and anonymously.
+          </p>
         </div>
-      </div>
+        <div className="grid gap-4 md:grid-cols-2">
+          <GetStarted
+            label={<Button>Get Started</Button>}
+          />
+          <RetrieveMesseges
+            label={
+              <Button variant="outline">
+                Retrive Messages
+              </Button>
+            }
+          />
+        </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="pb-20 md:pt-10">
+        <div className="container flex flex-wrap gap-8">
+          {HERO_INFO.map(({ title, content }, index) => (
+            <Card
+              className="flex-1 min-w-[240px]"
+              key={index}
+            >
+              <CardHeader>
+                <CardTitle className="text-lg">
+                  {title}
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="text-sm text-muted">
+                {content}
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </section>
     </main>
-  )
+  );
 }
+
+const GetStarted = ({
+  label,
+}: {
+  label: React.ReactNode;
+}) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{label}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create Account</DialogTitle>
+          <DialogDescription>
+            Enter a password and we&#39;ll create an
+            anonymous account for you with. If you already
+            have this an account, you can{' '}
+            <RetrieveMesseges
+              label={
+                <button className="text-secondary">
+                  proceed to login.
+                </button>
+              }
+            />
+          </DialogDescription>
+        </DialogHeader>
+        <form className="grid gap-6">
+          <div className="grid gap-2">
+            <label htmlFor="password">Password</label>
+            <Input
+              placeholder="Enter Password"
+              id="password"
+            />
+          </div>
+          <Button type="button" size="sm" className="mt-4">
+            Create Account
+          </Button>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const RetrieveMesseges = ({
+  label,
+}: {
+  label: React.ReactNode;
+}) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{label}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Login</DialogTitle>
+          <DialogDescription>
+            Login to retrieve your anonymous conversations.
+          </DialogDescription>
+        </DialogHeader>
+        <form className="grid gap-6">
+          <div className="grid gap-2">
+            <label htmlFor="id">User ID</label>
+            <Input placeholder="Enter user ID" id="id" />
+          </div>
+          <div className="grid gap-2">
+            <label htmlFor="password">Password</label>
+            <Input
+              placeholder="Enter Password"
+              id="password"
+            />
+          </div>
+          <Button type="button" size="sm" className="mt-4">
+            Login
+          </Button>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const HERO_INFO: { title: string; content: string }[] = [
+  {
+    title: 'Your Identity, Your Secret',
+    content: `Incognito ensures your identity remains hidden. 
+      Chat, share, and connect without revealing your 
+      personal information. Your secrets are safe with us.`,
+  },
+  {
+    title: 'Real Conversations, Real Connections',
+    content: `Engage in genuine conversations with 
+    people from around the world. Discover new friends, 
+    share stories, and explore the depths of human connection.`,
+  },
+  {
+    title: 'Endless Possibilities',
+    content: `Whether it's seeking advice, sharing your thoughts, 
+    or making new friends, Incognito opens the door to endless 
+    possibilities. You're in control of your conversations.`,
+  },
+  {
+    title: 'Join the Incognito Community Today',
+    content: `Unleash your voice and connect with others like never before. 
+    Join Incognito now and start your journey to anonymous conversations 
+    and authentic connections.`,
+  },
+];
